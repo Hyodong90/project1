69c3e62fe002a6bde0f6829fefc65a53d1bc8df3
Hyodong90 <ct99610138@gmail.com>
2025-09-26 01:21:04 +0900

    수정

--- dart_practice_1/bin/dart_practice_3.dart
+++ dart_practice_1/bin/dart_practice_3.dart
@@ -3,8 +3,6 @@ import 'dart:math';
 import 'package:dart_practice_3/dart_practice_3.dart' as dart_practice_3;
 
 Set<int> random = {};
-Set<int> lotto = {9, 19, 29, 35, 37, 38};
-
 
 void main() {
   while (random.length < 6) {
@@ -12,14 +10,4 @@ void main() {
   }
   print("발급한 로또 번호 : $random");
   print(random.length);
-
-  while (lotto =  ){
-
-  }
-
-
 }
-
-
-
-
