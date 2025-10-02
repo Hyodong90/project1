3acac5fdc22a9854eea24ef293900b224c87cd87
Hyodong90 <ct99610138@gmail.com>
2025-10-03 00:27:52 +0900

    과제2번시작

--- dart_practice/bin/dart_practice_1.dart
+++ dart_practice/bin/dart_practice_1.dart
@@ -1,16 +1,4 @@
 void main() {
-  int score = 84;
-  //level을 비워둬 등급이 들어가도록 함
-  String level = '';
 
-  //90점 이상 100점이하 둘다 충족되야 나오도록 구현
-  if (score >= 90 && score <= 100) {
-    level = 'A등급';
-  } else if (score >= 80 && score <= 89) {
-    level = 'B등급';
-  } else if (score < 80 && score >= 0) {
-    level = 'C등급';
-  }
 
-  print('이 학생의 점수는 $score점 이며, 등급은 $level입니다!');
 }
--- dart_practice/bin/dart_practice_2.dart
+++ dart_practice/bin/dart_practice_2.dart
@@ -1,30 +1,21 @@
 void main() {
   List<String> cart = ["티셔츠", "바지", "모자", "티셔츠", "바지"];
+  int total = 0;
+  Map<String, int> items = {"티셔츠": 10000, "바지": 8000, "모자": 4000};
 
-  // 각각 더할때도 하고 싶어 int값으로 정함.
-  // 근데 궁금한게 int값으로 다 만들어 놓으면 for문이 필요한지..
-  int tshirt = 10000;
-  int pants = 8000;
-  int hat = 4000;
-  int total = tshirt + pants + hat + tshirt + pants;
+  for (int i = 0; i < cart.length; i++) {
+    String item = cart[i];
 
-  // cart안에 아이템들을 하나씩 끌어와 이름을 만들어 주고 가격도 썼는데 위에 있는데 왜또한건지..
-  for (var item in cart) {
-    if (item == "티셔츠") {
-      tshirt = 10000;
-    } else if (item == "바지") {
-      pants = 8000;
-    } else if (item == "모자") {
-      hat = 4000;
-    }
+    total += items[item] ?? 0;
   }
-  //위에 total 만들어서 total로도 사용 가능하게 만듬!
+
   print("장바구니에 ${total}원 어치를 담으셨네요!");
 
-  //20000만원 이상일때 10%할인, 이하일때 else로 끝내고 출력안하기
   if (total >= 20000) {
-    print("할인 금액 : ${total / 10}원");
+    int discount = total ~/ 10;
+    print("할인 금액 : $discount원");
   } else {}
 
-  print("최종 결제 금액은 ${total - total / 10}원 입니다!");
+  var totalprice = total - total / 10;
+  print("최종 결제 금액은 $totalprice원 입니다!");
 }
